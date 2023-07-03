e0a06e19ded346a493210a02e1c0ddf2de055a2d
Arjun Sojan <avadakkekarayil-sojan@myseneca.ca>
2023-07-02 23:20:00 -0400

    Made change to include try and except

--- lab1.py
+++ lab1.py
@@ -1,14 +1,17 @@
 def calc_age():
   current_year = 2023
-  year = int(input("Please Enter the year of birth:"))
-  age = current_year - year
-  print ("Your age is :", age)
-<<<<<<< HEAD
+  try:
+        year = int(input("Please enter the year of birth: "))
+        age = current_year - year
+        print("Your age is:", age)
+  except TypeError:
+        print("Please enter an integer")
+
 def helloWorld():
   print("Hello World")
 
 helloWorld()
+calc_age()
 
-=======
->>>>>>> 43720de08cf93a512b84a969921d97e28099bccf
+
   
